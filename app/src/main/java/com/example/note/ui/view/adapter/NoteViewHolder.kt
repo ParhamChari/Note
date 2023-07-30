dc25a2ccb7b29545f0ce75642284826e59c6a183
Parham <parhamchary@gmail.com>
2023-07-31 04:09:31 +0430

    Update codes

--- app/src/main/java/com/example/note/ui/view/adapter/NoteViewHolder.kt
+++ app/src/main/java/com/example/note/ui/view/adapter/NoteViewHolder.kt
@@ -1,8 +1,10 @@
 package com.example.note.ui.view.adapter
 
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.note.data.model.NoteModel
 import com.example.note.databinding.NoteItemBinding
+import com.example.note.ui.view.fragments.HomeFragmentDirections
 
 class NoteViewHolder(private val binding: NoteItemBinding) : ViewHolder(binding.root){
 
@@ -10,6 +12,10 @@ class NoteViewHolder(private val binding: NoteItemBinding) : ViewHolder(binding.
         binding.apply {
             title.text = note.title
             description.text = note.description
+            cardView.setOnClickListener{view->
+                val direction = HomeFragmentDirections.actionHomeFragmentToUpdateFragment(note)
+                view.findNavController().navigate(direction)
+            }
         }
     }
 }
--- app/src/main/java/com/example/note/ui/view/fragments/UpdateFragment.kt
+++ app/src/main/java/com/example/note/ui/view/fragments/UpdateFragment.kt
@@ -5,25 +5,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.note.R
+import com.example.note.data.model.NoteModel
 import com.example.note.databinding.FragmentUpdateBinding
+import com.example.note.ui.view.activities.MainActivity
+import com.example.note.ui.viewmodel.NoteViewModel
+import com.google.android.material.snackbar.Snackbar
 
 class UpdateFragment : Fragment() {
 
-    lateinit var binding: FragmentUpdateBinding
+    private lateinit var binding: FragmentUpdateBinding
+    private val argument : UpdateFragmentArgs by navArgs()
+    private lateinit var currentNote : NoteModel
+    private lateinit var  noteViewModel: NoteViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout for this fragment
         binding = FragmentUpdateBinding.inflate(inflater,container,false)
-
-        binding.btnEditNote.setOnClickListener{
-            findNavController().navigate(R.id.action_updateFragment_to_homeFragment)
-        }
-
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bindViews()
+    }
+
+    private fun bindViews() {
+        noteViewModel = (activity as MainActivity).noteViewModel
+        currentNote = argument.note!!
+        binding.editTitle.setText(currentNote.title)
+        binding.editDescription.setText(currentNote.description)
+        binding.btnEditNote.setOnClickListener{
+            updateNote()
+        }
+    }
+
+    private fun updateNote() {
+        val noteTitle = binding.editTitle.text.toString().trim()
+        val noteDesc = binding.editDescription.text.toString().trim()
+        if (noteTitle.isNotEmpty() && noteDesc.isNotEmpty()) {
+            val note = NoteModel(currentNote.id, noteTitle, noteDesc)
+            noteViewModel.updateNote(note)
+            Snackbar.make(binding.root, "داده مورد نظر بروز شد", Snackbar.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_updateFragment_to_homeFragment)
+        } else {
+            Toast.makeText(requireContext(), "لطفا اطلاعات را وارد کنید", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
